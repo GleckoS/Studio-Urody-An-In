6102673585b517b7dec21e223539099a03d52eb9
Glecko <shevabogdan16@gmail.com>
2020-10-13 21:00:24 +0200

    text

--- js/main.js
+++ js/main.js
@@ -31,15 +31,8 @@ $('.services__items').slick({
         {
             breakpoint: 1024,
             settings: {
-                slidesToShow: 3,
-                slidesToScroll: 3,
-            }
-        },
-        {
-            breakpoint: 769,
-            settings: {
                 slidesToShow: 2,
-                slidesToScroll: 2
+                slidesToScroll: 2,
             }
         },
         {
@@ -50,6 +43,12 @@ $('.services__items').slick({
             }
         }]
 })
+
+
+const Fryzjer = [
+    {name: "Klasyczne bikini", cena: "150zł" },{name: "Brazylijskie bikini", cena: "180zł"},{name: "Łydki", cena: "250zł"}
+]
+
 
 const animItems = document.querySelectorAll(`._anim-items`)
 if (animItems.length > 0) {
